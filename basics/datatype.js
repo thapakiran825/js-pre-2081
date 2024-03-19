159671a3bd852a58257affec34e83ca6bff07249
thapakiran825 <59519903+thapakiran825@users.noreply.github.com>
2024-03-19 16:58:59 +0000

    datatypes

---
+++ basics/datatype.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+//alert ( 3 + 3 ) //we are using nodejs, not browser
+
+console.log(3 + 3)
+
+console.log("Kiran")
+
+let name = 'Kiran'
+let age = 25
+let isLoggedIn = false
+let state; //undefined
+
+//premitive datatypes:: 
+//number  => 2 to power 53
+//bigint
+//string => ''or""
+//boolean => true/false
+//null => standalone value
+//undefined => value not assigned
+//symbol => mostly used for uniqueness
+
+//object
+console.log(typeof isLoggedIn)
+console.log(typeof null)
+console.log(typeof undefined)
